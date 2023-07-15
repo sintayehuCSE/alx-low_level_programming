e3785ed98d830d5f6310a170e7c58c09fc512ec5
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-07-14 20:12:22 -0700

    A program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,13 +14,13 @@ for (i = 0; i <= 9; i++)
 {
 	for (j = 0; j <= 9; j++)
 	{
-		putchar(i + 48);
-		putchar(j + 48);
-		putchar(' ');
 		for (k = i; k <= 9; k++)
 		{
 			for (m = j + 1; m <= 9; m++)
 			{
+				putchar(i + 48);
+                		putchar(j + 48);
+                		putchar(' ');
 				putchar(k + 48);
 				putchar(m + 48);
 				if ((i == 9) && (j == 8) && (k == 9) && (m == 9))
