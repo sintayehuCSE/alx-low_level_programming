95f3e7e6b5340e8bf13e206f402589b26ca665c4
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-07-18 14:47:52 -0700

    A C function that prints all natural numbers from n to 98, followed by a new line

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,9 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
 			n--;
 		}
 	}
@@ -18,8 +20,11 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+				printf(, );
 			n++;
 		}
 	}
+	printf("\n");
 }
