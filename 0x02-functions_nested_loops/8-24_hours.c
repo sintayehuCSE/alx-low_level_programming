e9ab5c7c15df142206b8274a1b51c9935d03acc5
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-07-18 12:55:01 -0700

    A C function that prints every minute of the day, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every time and minute of a day
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 23)
+	{
+		while (j <= 59)
+		{
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			_putchar(58);
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar(10);
+			j++;
+		}
+		i++;
+	}
+}
