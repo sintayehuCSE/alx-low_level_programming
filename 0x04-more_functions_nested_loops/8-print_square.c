02e3c37bbe329900f18ef0598c69b6ed54105f05
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-07-20 17:45:13 -0700

    A c function that prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - print out a square
+ * @size: size of a squaree
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			for (; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
