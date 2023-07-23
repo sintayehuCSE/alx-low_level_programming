630cadc3765b15661a6ec6b87b3e44963f651098
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-07-23 09:50:56 -0700

    A c  program that finds and prints the largest prime factor of a number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,19 +1,68 @@
 #include <stdio.h>
-#include "main.h"
+long int largest_prime_factor(long int);
+int check_prime(long int)
 /**
- * main - prints the largest prime factor of 612852475143
- * Return: always 0
- **/
+ * main - prints the largest prime factor of specified no.
+ * Return: 0 on exit
+ */
 
-int main(void)
+int main(void);
 {
-	unsigned long int i, n = 612852475143;
+	long int num = 612852475143;
+	long int largest = largest_prime_factor(num);
 
-	for (i = 3; i < 782849; i = i + 2)
-	{
-		while ((n % i == 0) && (n != i))
-			n = n / i;
-	}
-	printf("%lu\n", n);
+	printf("%ld\n", largest);
 	return (0);
+}
+
+/**
+ * largest_prime_factor - finds the largest prime number
+ * @n: number whose largest prime factor is to be determined
+ * Return: largest prime number
+ */
+long int largest_prime_factor(long int n)
+{
+	long int i = 2;
+	long int old = 2;
+	long int current;
+	long int largest;
+	int result;
+
+	for (; i <= n;)
+	{
+		result = check_prime(i);
+		if ((result == 1) && (n % i == 0))
+		{
+			current = i;
+			if (current >= old)
+			{
+				largest = current;
+			}
+			old = current;
+			n = n / i;
+			i = 2;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (largest);
+}
+/**
+ * check_prime - check if a number is prime
+ * @n: number to check
+ * Return: 1 if true. 0 otherwise
+ */
+
+int check_prime(long int n)
+{
+	int a = 2;
+
+	for (; a < n; a++)
+	{
+		if (n % a == 0)
+			return (0);
+	}
+	return (1);
 }
