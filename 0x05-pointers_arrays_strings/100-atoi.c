abfa9b24b0f7fbeec9bc794a04437cfd147dc3fa
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-07-24 19:24:51 -0700

    A c function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,12 +13,12 @@ int _atoi(char *s)
 
 	while (*s)
 	{
-		if ((*s >= 48) && (*s <= 57))
+		if (((*s) >= 48) && ((*s) <= 57))
 		{
-			if (*(s - 1) == 45)
+			if ((*(s - 1)) == 45)
 				check *= -1;
-			result = (result + (*s)) * 10;
-			if ((*(s + 1) == 32) || (*(s + 1) == 0))
+			result = (result + (int)(*s)) * 10;
+			if (((*(s + 1)) == 32) || ((*(s + 1)) == 0))
 			{
 				if (check == -1)
 					return (-1 * (result / 10));
