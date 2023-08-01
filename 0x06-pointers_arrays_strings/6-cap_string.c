5808cbad1f99dcc4a1c4ce6639cc224140930d09
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-08-01 03:27:07 -0700

    A c function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,66 @@
 #include "main.h"
-
+int iscapital(char *);
+int ischar(char *);
 /**
- * cap_string - capitalizes all words in a string
- * @s: string
- * Return: address of s
+ * cap_string - capitalize each word of a string
+ * @str: string whose word is to be capitalized
+ *
+ * Return: char pointer to capitalized string
  */
-char *cap_string(char *s)
-{
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"(){}";
 
-	while (*(s + i))
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char term[] = {32, 10, 9, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int check_if_char;
+	int check_capital;
+
+	while (*(str + i))
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		check_if_char = ischar((str + i));
+		if (check_if_char == 1)
 		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
+			check_capital = iscapital((str + i));
+			if (check_capital == 0)
 			{
-				for (j = 0; j <= 12; j++)
+				for (; term[j]; j++)
 				{
-					if (a[j] == *(s + i - 1))
-						*(s + i) -= 'a' - 'A';
+					if (*(str + i - 1) == term[j])
+					{
+						*(str + i) -= 32;
+						break;
+					}
 				}
+				j = 0;
 			}
 		}
 		i++;
 	}
-	return (s);
+	return (str);
+}
+/**
+ * ischar - check if a character is alphabet
+ * @c: character to be checked
+ *
+ * Return: 1 if alphabet. 0 otherwise
+ */
+int ischar(char *c)
+{
+	if ((*c >= 97 && *c <= 122) || (*c >= 65 && *c <= 90))
+		return (1);
+	return (0);
+}
+/**
+ * iscapital - check if character is capital letter
+ * @c: character to be checked
+ *
+ * Return: 1 if true. 0 otherwise
+ */
+
+int iscapital(char *c)
+{
+	if ((*c <= 90) && (*c >= 65))
+		return (1);
+	return (0);
 }
