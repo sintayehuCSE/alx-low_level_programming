669e326697b3af2cb1270a1c3e3cf72c92f7ec67
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-08-05 11:26:15 -0700

    A c function that prints a string recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * _puts_recursion - print a string
- * @s:string
- * Return:void
+ * _puts_recursion - prints string to std output
+ * @s: pointer to the string to be printed
+ *
+ * Return: nothing
  */
+
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
-else
-{
-	_putchar('\n');
-}
-
+	if (*s)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar(10);
 }
