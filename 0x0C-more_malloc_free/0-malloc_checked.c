3c5db07d95504a05bb31d7f5a17fdebebc85b9ca
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-08-10 14:36:30 -0700

    A c function that allocat memory using malloc func and if failed cause normal process termination with status value of 98

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc fun.
+ * @b: The size of memory to be allocated by malloc func in byte
+ *
+ * Return: Pointer to the allocated space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
