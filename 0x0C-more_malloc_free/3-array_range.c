ce83cc09eaf0b3424192493f93cbf5734b93b2d8
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-08-11 06:47:14 -0700

    A c function that creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,4 +1,50 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int *a;
+
+    a = array_range(0, 10);
+    simple_print_buffer(a, 11);
+    free(a);
+    return (0);
+}
 /**
  * array_range - create an array of integers
  * @min: minimum integre element of an array
@@ -13,7 +59,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	*ptr = malloc(sizeof(int) * ((max - min) + 1));
+	ptr = malloc(sizeof(int) * ((max - min) + 1));
 	if (ptr == NULL)
 		return (NULL);
 	while (min <= max)
