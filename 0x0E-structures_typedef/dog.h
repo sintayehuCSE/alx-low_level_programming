3b9301f8d76b611998bc1ae3da1cb33a67516f5d
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-08-14 17:46:22 -0700

    A header file that define struct type of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - define a new type struct dog
+ * @name: A char pointer to a name of dog
+ * @age: Age of a new type struct dog
+ * @owner: The owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
