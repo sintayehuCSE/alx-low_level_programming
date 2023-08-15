7d75919ed5ca41abb5b70d6a696c82c693513f10
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-08-14 18:56:52 -0700

    Update dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,14 @@
  * @owner: The owner of the dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
 #endif /* DOG_H */
