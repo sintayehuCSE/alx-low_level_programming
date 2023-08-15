570077fd67bfdc57875064d11371a9049117a9c7
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-08-14 17:24:35 -0700

    A function-like macro ABS(x) that computes the absolute value of a number x

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef ABS
-#define ABS
+#ifndef _3_function_like_macro_h
+#define _3_function_like_macro_h
 
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif /* ABS */
+#endif /* _3_function_like_macro_h */
