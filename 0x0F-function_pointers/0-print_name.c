2300aed2625ff4fb689b8650b7922e2c3582d34a
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-08-17 03:30:10 -0700

    A c function that prints string name passed to it

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,10 @@
 #include "function_pointers.h"
-
 /**
- * print_name - a function that prints a name.
- * @name: the name given
- * @f: function of name
- *
- * Rteurn: void
+ * print_name - A function that print string name passed to it
+ * @name: A pointer to the name to be printed
+ * @f: A pointer to function that does the printing job
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-
-		f(name);
+	f(name);
 }
