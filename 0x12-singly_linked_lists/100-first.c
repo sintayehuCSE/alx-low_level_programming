5c3e9174299cef6bd2186a39686de67dabb9e2a4
Sintayehu Mulugeta <=>
2023-08-30 04:20:09 -0700

    A function that executes before a main function execution begins

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,8 @@
 /**
  * before_main - Performs some task before main function execution begins
  */
-void before_main()
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
