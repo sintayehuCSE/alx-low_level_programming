4a054c32df41d7554eafd167ccb3afd5778f633a
Sintayehu Mulugeta <=>
2023-08-30 02:59:14 -0700

    A function that add new node to the end of a list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,60 +1,6 @@
 #include "lists.h"
 char *_strdup(const char *);
 unsigned int _strlen(const char *);
-
-size_t print_list(const list_t *h)
-{
-	while (h)
-	{
-		if (h->next == NULL)
-		{
-			printf("[%u] %s\n", h->len, h->str);
-			write(1, "[0] (nil)\n", 10);
-		}
-		else
-		{
-			printf("[%u] %s\n", h->len, h->str);
-		}
-		h = h->next;
-	}
-	return (0);
-}
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    add_node_end(&head, "Anne");
-    add_node_end(&head, "Colton");
-    add_node_end(&head, "Corbin");
-    add_node_end(&head, "Daniel");
-    add_node_end(&head, "Danton");
-    add_node_end(&head, "David");
-    add_node_end(&head, "Gary");
-    add_node_end(&head, "Holden");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Jay");
-    add_node_end(&head, "Jennie");
-    add_node_end(&head, "Jimmy");
-    add_node_end(&head, "Justin");
-    add_node_end(&head, "Kalson");
-    add_node_end(&head, "Kina");
-    add_node_end(&head, "Matthew");
-    add_node_end(&head, "Max");
-    add_node_end(&head, "Michael");
-    add_node_end(&head, "Ntuj");
-    add_node_end(&head, "Philip");
-    add_node_end(&head, "Richard");
-    add_node_end(&head, "Samantha");
-    add_node_end(&head, "Stuart");
-    add_node_end(&head, "Swati");
-    add_node_end(&head, "Timothy");
-    add_node_end(&head, "Victor");
-    add_node_end(&head, "Walton");
-    print_list(head);
-    return (0);
-}
 /**
  * add_node_end - Add a new node to the end of a singly linked list
  * @head: Pointer to the list
