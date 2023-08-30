388ac3a681ebe520e1319843dfff20684fdac6d0
Sintayehu Mulugeta <=>
2023-08-30 03:26:41 -0700

    A function that release a memory allocated to list nodes

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
 /**
  * free_list - Realease the memory allocated for a list
- *
  * @head: A pointer to the first node of the list to free
  */
 void free_list(list_t *head)
 {
-	if (head)
+	while (head)
 	{
-		free_list(head->next);
+		free_list(head_next);
 		if (head->str)
 			free(head->str);
 		free(head);
