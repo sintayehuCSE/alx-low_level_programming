6a3b151288dffd6fddb21d5d0b318aba7758ab4e
Sintayehu Mulugeta <=>
2023-08-30 02:55:06 -0700

    A function that add new node to the end of a list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,54 +1,127 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+char *_strdup(const char *);
+unsigned int _strlen(const char *);
 
-/**
- * _strlen - finds the length of a string
- * @str: string to find the length of
- *
- * Return: length of string
- */
-unsigned int _strlen(char *str)
+size_t print_list(const list_t *h)
 {
-	unsigned int i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
+	while (h)
+	{
+		if (h->next == NULL)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			write(1, "[0] (nil)\n", 10);
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (0);
 }
+int main(void)
+{
+    list_t *head;
 
+    head = NULL;
+    add_node_end(&head, "Anne");
+    add_node_end(&head, "Colton");
+    add_node_end(&head, "Corbin");
+    add_node_end(&head, "Daniel");
+    add_node_end(&head, "Danton");
+    add_node_end(&head, "David");
+    add_node_end(&head, "Gary");
+    add_node_end(&head, "Holden");
+    add_node_end(&head, "Ian");
+    add_node_end(&head, "Ian");
+    add_node_end(&head, "Jay");
+    add_node_end(&head, "Jennie");
+    add_node_end(&head, "Jimmy");
+    add_node_end(&head, "Justin");
+    add_node_end(&head, "Kalson");
+    add_node_end(&head, "Kina");
+    add_node_end(&head, "Matthew");
+    add_node_end(&head, "Max");
+    add_node_end(&head, "Michael");
+    add_node_end(&head, "Ntuj");
+    add_node_end(&head, "Philip");
+    add_node_end(&head, "Richard");
+    add_node_end(&head, "Samantha");
+    add_node_end(&head, "Stuart");
+    add_node_end(&head, "Swati");
+    add_node_end(&head, "Timothy");
+    add_node_end(&head, "Victor");
+    add_node_end(&head, "Walton");
+    print_list(head);
+    return (0);
+}
 /**
- * add_node_end - adds a new node to the end of linked list
- * @head: double pointer to a linked list
- * @str: string to add to the new node
+ * add_node_end - Add a new node to the end of a singly linked list
+ * @head: Pointer to the list
+ * @str: Pointer to string value of the list
  *
- * Return: pointer to the new node
+ * Return: Pointer to the list
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *tmp;
+	list_t *temp;
+	list_t *new;
 
-	if (str == NULL)
+	temp = malloc(sizeof(*temp));
+	if (!temp)
 		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	if (new->str == NULL)
+	temp->str = _strdup(str);
+	temp->len = _strlen(str);
+	temp->next = NULL;
+	if (!*head)
 	{
-		free(new);
-		return (NULL);
+		*head = temp;
+		return (temp);
 	}
-	new->len = _strlen(new->str);
-	new->next = NULL;
-	if (*head == NULL)
+	new = *head;
+	while (new->next)
 	{
-		*head = new;
-		return (new);
+		new = new->next;
 	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
+	new->next = temp;
 	return (new);
+}
+/**
+ * _strdup - Duplicate a string for storing as struct memeber
+ * @str: The string to be duplicated
+ *
+ * Return: The pointer tot duplicated string
+ */
+char *_strdup(const char *str)
+{
+	char *s;
+	int len = _strlen(str);
+
+	s = malloc(sizeof(*s) * (len + 1));
+	if (!s)
+		return (NULL);
+	while (len >= 0 && str)
+	{
+		s[len] =  str[len];
+		len--;
+	}
+	if (s)
+		return (s);
+	return (NULL);
+}
+/**
+ * _strlen - Determine the length of a string
+ * @str: Pointer to the string
+ *
+ * Return: Length of a string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
+
+	while (str && str[len])
+	{
+		len++;
+	}
+	return (len);
 }
