e4b997ec9f8f61b72e168903e74f4730842fdf01
Sintayehu Mulugeta <=>
2023-08-31 02:00:30 -0700

    A function that add a node to the end of a singly linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,34 +1,29 @@
 #include "lists.h"
-#include <stdlib.h>
 /**
-  * add_nodeint_end - add node at end of a listint_t list.
-  *
-  * @head: head of double pointer
-  * @n: int add the list
-  * Return: NULL if it failed
-  */
+ * add_nodeint_end -  Add a node to the end of singly linked list
+ * @head: A pointer to the linked list
+ * @n: The integer to store
+ *
+ * Return: Pointer to the new node
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
 	listint_t *temp;
+	listint_t *last_node;
 
-	if (head == NULL)
+	temp = malloc(sizeof(*temp));
+	if (!temp)
 		return (NULL);
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (*head == NULL)
+	temp->n = n;
+	temp->next = NULL;
+	if (!*head)
+		*head = temp;
+	else
 	{
-	*head = new;
-		return (new);
+		last_node = *head;
+		while (last_node->next)
+			last_node = last_node->next;
+		last_node->next = temp;
 	}
-	temp = *head;
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	temp->next = new;
-	return (new);
+	return (temp);
 }
