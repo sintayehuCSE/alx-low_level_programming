a22c51d8310df4897c5a5cd2bf5500307aff32a7
Sintayehu Mulugeta <=>
2023-08-31 03:28:13 -0700

    A function that searches for the specified node of a singly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,23 +1,21 @@
 #include "lists.h"
-
 /**
- * get_nodeint_at_index - the nth node of a listint_t linked list.
- * @head: pointer of the first node
- * @index: index is the index of the node
+ * get_nodeint_at_index - Finds the specified node of a singly linked list
+ * @head: Pointer to the singly linked list
+ * @index: The location of the desired node
  *
- * Return: pointer of Index
+ * Return: Pointer to the located node or NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
-	for (i = 0; i < index; i++)
+	for (; i < index, head;)
 	{
 		head = head->next;
-		if (head == NULL)
-			return (NULL);
+		i++;
 	}
 	return (head);
 }
