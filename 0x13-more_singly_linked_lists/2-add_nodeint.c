e1e39ca1c4820673e3e32c9597e87d8e98571a75
Sintayehu Mulugeta <=>
2023-08-30 23:14:27 -0700

    A function that add new node to the beggining of a singly linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,23 +1,22 @@
-#include "lists.h"
-#include <stdlib.h>
+#include "main.h"
 /**
-  * add_nodeint - add node at beginning of a listint_t list.
-  *
-  * @head: head of double pointer
-  * @n: int add the list
-  * Return: NULL if it failed
-  */
+ * add_nodeint - Add a new node to a linked list
+ * @head: Pointer to the singly linked list
+ * @n: The integer data
+ *
+ * Return: Pointer to the new node
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *ptr;
+	listint_t *temp;
 
-	if (head == NULL)
+	temp = malloc(sizeof(*temp));
+	if (!temp)
+	{
 		return (NULL);
-	ptr = malloc(sizeof(listint_t));
-	if (ptr == NULL)
-		return (NULL);
-	ptr->n = n;
-	ptr->next = *head;
-	*head = ptr;
-	return (ptr);
+	}
+	temp->n = n;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }
