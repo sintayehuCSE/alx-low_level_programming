b493abf1d250e6ab465e36dcef3caca9cccd6d09
Sintayehu Mulugeta <=>
2023-08-30 22:38:05 -0700

    A function that list all element of singly linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,19 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * print_listint - prints all the elements of a listint_t list.
+ * print_listint - Print each element of a singly linked list
+ * @h: Pointer to the singly linked list
  *
- * @h: head of linklist node
- *
- * Return: the number of nodes
- *
+ * Return: The number of nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t node_num = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
+		node_num++;
 		h = h->next;
-		count++;
 	}
-	return (count);
+	return (node_num);
 }
