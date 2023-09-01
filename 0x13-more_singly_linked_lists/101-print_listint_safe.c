db78462148f3c4fe282460c043150d0f6e949701
Sintayehu Mulugeta <=>
2023-09-01 08:20:04 -0700

    Trying

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,61 +1,81 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
- * _r - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list
+ * print_listint_safe - Print list in safe mode
+ * @head: Pointer to the list
  *
- * Return: pointer to the new list
- */
-const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
-{
-	const listint_t **newlist;
-	size_t i;
-
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
-	{
-		free(list);
-		exit(98);
-	}
-	for (i = 0; i < size - 1; i++)
-		newlist[i] = list[i];
-	newlist[i] = new;
-	free(list);
-	return (newlist);
-}
-
-/**
- * print_listint_safe - prints a listint_t linked list.
- * @head: pointer to the start of the list
- *
- * Return: the number of nodes in the list
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i, num = 0;
-	const listint_t **list = NULL;
+	size_t num_node = 0;
+	listint_t *slow_ptr;
+	listint_t *fast_ptr;
+	listint_t *jam_loop;
 
-	while (head != NULL)
+	if (!head->next)
 	{
-		for (i = 0; i < num; i++)
+		num_node = print_non_loop(head);
+		return (num_node);
+	}
+	slow_ptr = fast_ptr = head;
+	while (slow_ptr && fast_ptr && fast_ptr->next)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+		if (slow_ptr == fast_ptr)
 		{
-			if (head == list[i])
+			slow_ptr = head;
+			while (slow_ptr != fast_ptr)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free(list);
-				return (num);
+				slow_ptr = slow_ptr->next;
+				fast_ptr = fast_ptr->next;
+				if (slow_ptr == fast_ptr)
+				{
+					jam_loop = head;
+					while (jam_loop != fast_loop)
+					{
+						printf("[%p] %d\n", jam_loop->next, jam_loop->n);
+						num_node++;
+						jam_loop = jam_loop->next;
+					}
+					printf("[%p] %d\n", jam_loop->next, jam_loop->n);
+					jam_loop = jam_loop->next;
+					while (jam_loop != slow_loop)
+					{
+						printf("[%p] %d\n", jam_loop->next, jam_loop->n);
+						num_node++;
+						jam_loop = jam_loop->next;
+					}
+					printf("-> [%p] %d\n", jam_loop->next, jam_loop->n);
+				}
 			}
 		}
-		num++;
-		list = _r(list, num, head);
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
+		if (slow_ptr == fast_ptr)
+			break;
+		if (!fast_ptr)
+			num_node = print_non_loop(head);
 	}
-	free(list);
-	return (num);
+	if (slow_ptr == fast_ptr && fast_ptr == head)
+		exit (98);
+	return (num_node);
+}
+/**
+ * print_non_loop - Prints singly linked list with no loop
+ * @head: Pointer to the non-looped singly linked list
+ *
+ * Return: Number of node in the list
+ */
+size_t print_non_loop(listint_t *head)
+{
+	size_t num_node = 0;
+	long int node_addr = (void *)head;
+
+	while (head)
+	{
+		printf("[%ld] %d\n", node_addr, head->n);
+		num_node++;
+		head = head->next;
+		node_addr = (void *)head;
+	}
+	return (num_node);
 }
