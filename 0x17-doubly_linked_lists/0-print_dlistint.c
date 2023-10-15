31389e0bb8d311ea0053494f4f8aeeff2d945ade
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-10-15 20:24:45 +0300

    A c function that prints all element of doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,11 +10,12 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *temp;
 	size_t n = 0;
 
-	if (!*head)
+	if (!*h)
 		return (n);
 	temp = malloc(sizeof(dlistint_t));
 	if (!temp)
 		return (n);
+	temp = h;
 	while (temp)
 	{
 		printf("%d\n", temp->n);
