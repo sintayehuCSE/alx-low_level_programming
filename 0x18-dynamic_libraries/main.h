f6c7e16b583e6170250762c78b7ba7b3b8501927
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2023-10-17 02:26:03 +0300

    This function need to be in their original place

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -21,4 +21,11 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+/**............Function to be called in python code ...........**/
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
 #endif /* MAIN_H */
