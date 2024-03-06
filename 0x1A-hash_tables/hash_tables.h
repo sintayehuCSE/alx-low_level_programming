ad46e6212eef905c61b69869fa3d80725f62170a
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-03-06 23:57:07 +0300

    Database of HashTable

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,5 +38,6 @@ typedef struct hash_table_s
 
 /*...................Hash Table functions...................*/
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /** Hash_Table*/
